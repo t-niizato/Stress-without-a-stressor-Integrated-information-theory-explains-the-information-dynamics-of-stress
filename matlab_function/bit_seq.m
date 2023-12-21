51df3f4446088c134c3c2e2c801984ceb2a11dba
t-niizato <153797597+t-niizato@users.noreply.github.com>
2023-12-21 13:34:27 +0900

    Delete matlab_function directory

--- matlab_function/bit_seq.m
+++
@@ -1,7 +0,0 @@
-function [result] = bit_seq(Data)
-% 二進法から十進法
-    bitnum = size(Data, 2)-1;
-    seq = 2.^(0:bitnum);
-    result = sum(Data.*seq,2);
-end
-
--- matlab_function/unique_without_nan.m
+++
@@ -1,7 +0,0 @@
-function [y] = unique_without_nan(x)
-  y = unique(x);
-  if any(isnan(y))
-    y(isnan(y)) = []; % remove all nans
-  end
-
-end
